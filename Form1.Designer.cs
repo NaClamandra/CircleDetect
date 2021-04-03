2e82ba7cf1fe367077a104367736be17b79bffe5
NaClamandra <74439320+NaClamandra@users.noreply.github.com>
2021-04-03 15:35:34 -0600

    Eficientado

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace CircleDetect
+{
+    partial class Form_Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(84, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(806, 630);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(213, 634);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Cargar";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(684, 634);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Detectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(937, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(335, 214);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(937, 316);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(335, 214);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1037, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Orden por Tamaño (Bubble)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1037, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Orden por ID (Original)";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(1334, 66);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(334, 214);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1415, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Orden por aparición Eje X (Selection)";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(1334, 316);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(334, 214);
+            this.listBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1415, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Orden por aparición Eje Y (Insertion)";
+            // 
+            // Form_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1759, 705);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form_Principal";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Detectar Circulo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button button2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleDetect
+{
+    public partial class Form_Principal : Form
+    {
+        Color Black = Color.FromArgb(255, 0, 0, 0);
+        Color White = Color.FromArgb(255, 255, 255, 255);
+        int circulo = 0;
+        Bitmap copia;
+        List<Circulo> Circulos = new List<Circulo>();
+        List<Circulo> BubbleCirc = new List<Circulo>();
+        List<Circulo> SelectCirc = new List<Circulo>();
+        List<Circulo> InsertCirc = new List<Circulo>();
+
+        public Form_Principal()
+        {
+            InitializeComponent();
+        }
+        //Bubble sort
+        public static void exchange(List<Circulo> circulos, int m, int n)
+        {
+            Circulo temporary;
+
+            temporary = circulos[m];
+            circulos[m] = circulos[n];
+            circulos[n] = temporary;
+        }
+        public static void BubbleSort(List<Circulo> circulos)
+        {
+            int i, j;
+            int N = circulos.Count;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (circulos[i].area < circulos[i + 1].area)
+                        exchange(circulos, i, i + 1);
+                }
+            }
+        }
+        //Selection sort
+        public static int Min(List<Circulo> circulo, int start)
+        {
+            int minPos = start;
+            for (int pos = start + 1; pos < circulo.Count; pos++)
+                if (circulo[pos].centrox < circulo[minPos].centrox)
+                    minPos = pos;
+            return minPos;
+        }
+        public static void SelectionSort(List<Circulo> circulo)
+        {
+            int i;
+            int N = circulo.Count;
+
+            for (i = 0; i < N - 1; i++)
+            {
+                int k = Min(circulo, i);
+                if (i != k)
+                    exchange(circulo, i, k);
+            }
+        }      
+        //Insertion sort
+        public static void InsertionSort(List<Circulo> circulo)
+        {
+            int i, j;
+            int N = circulo.Count;
+
+            for (j = 1; j < N; j++)
+            {
+                for (i = j; i > 0 && circulo[i].centroy < circulo[i - 1].centroy; i--)
+                {
+                    exchange(circulo, i, i - 1);
+                }
+            }
+        }
+
+        public void DetC(int j, int i, Bitmap img)
+        {
+            int pixelCount = 0, mitad, centro;
+            while (img.GetPixel(j, i) != White)
+            {
+                pixelCount += 1;
+                j++;
+            }
+            if (pixelCount % 2 != 0 && pixelCount != 0)
+            {
+                pixelCount = ((pixelCount - 1) / 2) + 1;
+                mitad = j - pixelCount;
+            }
+            else
+            {
+                pixelCount = pixelCount / 2;
+                mitad = j - pixelCount - 1;
+            }
+            pixelCount = 0;
+            while (img.GetPixel(mitad, i) != White)
+            {
+                pixelCount += 1;
+                i++;
+            }
+            if (pixelCount % 2 != 0 && pixelCount != 0)
+            {
+                pixelCount = ((pixelCount - 1) / 2) + 1;
+                centro = i - pixelCount;
+            }
+            else
+            {
+                pixelCount = pixelCount / 2;
+                centro = i - pixelCount - 1;
+            }
+
+            Graphics g = Graphics.FromImage(img);
+            Graphics h = Graphics.FromImage(copia);
+
+            Pen otherPen = new Pen(Color.Red,5);
+            SolidBrush white = new SolidBrush(White);
+
+            g.FillEllipse(white, mitad - pixelCount-2, centro - pixelCount-1, pixelCount * 2+4, pixelCount * 2+4);
+            h.DrawEllipse(otherPen, mitad - pixelCount , centro - pixelCount , pixelCount * 2 , pixelCount * 2 );
+            circulo += 1;
+            String drawString = circulo.ToString();
+            Font drawFont = new Font("Arial", 20);
+            SolidBrush drawBrush = new SolidBrush(Color.Orange);
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+
+            // Draw string to screen.
+            h.DrawString(drawString, drawFont, drawBrush, mitad +10, centro+10, drawFormat);
+            copia.SetPixel(mitad, centro, Color.Green);
+            copia.SetPixel(mitad, centro + 1, Color.Green);
+            copia.SetPixel(mitad - 1, centro, Color.Green);
+            copia.SetPixel(mitad + 1, centro, Color.Green);
+            copia.SetPixel(mitad, centro - 1, Color.Green);
+            copia.SetPixel(mitad, centro+2, Color.Green);
+            copia.SetPixel(mitad - 2, centro, Color.Green);
+            copia.SetPixel(mitad+2, centro, Color.Green);
+            copia.SetPixel(mitad, centro - 2, Color.Green);
+
+            double area = (3.1416 * (pixelCount * pixelCount));
+
+            Circulo circo = new Circulo(circulo,mitad,centro,pixelCount,area);
+            Circulos.Add(circo);
+            pictureBox1.Image = copia;         
+        }
+
+        public bool FindCirc(Bitmap img, int inicioy=0)
+        {
+            int h = img.Height;
+            bool buscar=true;
+            int w = img.Width;
+            Color bit;
+            for (int i = inicioy; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    bit = img.GetPixel(j, i);
+                    if (buscar && bit.R == bit.G && bit.B == bit.R && bit.R != 255)
+                    {
+                        DetC(j, i, img);
+                        return FindCirc(img,i);      
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Archivos de imagen|*.jpg;*.png";
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+                button2.Enabled = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            var img = (Bitmap)pictureBox1.Image;
+            copia = (Bitmap)img.Clone();
+            FindCirc((Bitmap)pictureBox1.Image);
+            circulo = 0;
+            listBox1.DataSource = null;
+            listBox1.DataSource = Circulos ;
+            BubbleCirc = Circulos;
+            SelectCirc = Circulos;
+            InsertCirc = Circulos;
+
+            BubbleSort(BubbleCirc);
+            listBox2.DataSource = null;
+            listBox2.DataSource = BubbleCirc;
+
+            SelectionSort(SelectCirc);
+            listBox3.DataSource = null;
+            listBox3.DataSource = SelectCirc;
+
+            InsertionSort(InsertCirc);
+            listBox4.DataSource = null;
+            listBox4.DataSource = InsertCirc;
+
+            button2.Enabled = false;
+            Circulos.Clear();
+        }
+    }
+}
